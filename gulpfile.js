3bb3ac36a79eb11bd3c2486d27dcbc1e46fbab5a
luchjr <rockwindsea@gmail.com>
2022-05-28 15:44:00 +0300

    two sections

--- gulpfile.js
+++ gulpfile.js
@@ -34,39 +34,49 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", gulp.parallel('html'))
+    gulp.watch("src/*.html").on("change", gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("src/fonts/**/*").on('add', gulp.parallel('fonts'));
+    gulp.watch("src/icons/**/*").on('add', gulp.parallel('icons'));
+    gulp.watch("src/img/**/*").on('add', gulp.parallel('images'));
 });
 
 gulp.task('html', function() {
     return gulp.src("src/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist/"));
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function() {
     return gulp.src("src/js/**/*.js")
-    .pipe(gulp.dest("dist/js"));
+    .pipe(gulp.dest("dist/js"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('fonts', function() {
     return gulp.src("src/fonts/**/*")
-    .pipe(gulp.dest("dist/fonts"));
+    .pipe(gulp.dest("dist/fonts"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('icons', function() {
     return gulp.src("src/icons/**/*")
-    .pipe(gulp.dest("dist/icons"));
+    .pipe(gulp.dest("dist/icons"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('mailer', function() {
     return gulp.src("src/icons/**/*")
-    .pipe(gulp.dest("dist/mailer"));
+    .pipe(gulp.dest("dist/mailer"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('images', function() {
     return gulp.src("src/img/**/*")
     .pipe(imagemin())
-    .pipe(gulp.dest("dist/img"));
+    .pipe(gulp.dest("dist/img"))
+    .pipe(browserSync.stream());
 });
 
 
